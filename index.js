df3ce95b162800aec69e51fac48c79d62aeeee72
System Administrator <root@Keiths-MacBook-Pro.local>
2019-05-13 17:57:40 -0400

    repositioned routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(require('./routes'));
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
 app.use(express.static('public'));
 app.set('view engine', 'pug');
